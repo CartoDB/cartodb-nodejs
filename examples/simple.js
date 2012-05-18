35cef1a6737dfcac170f5e27d98a869bc3327e54
javi <qualopec@gmail.com>
2012-05-18 12:15:46 +0200

    added some example

---
+++ examples/simple.js
@@ -0,0 +1,27 @@
+
+var cartodb = require('../');
+var secret = require('./secret.js');
+
+
+var client = new cartodb.CartoDBClientApiKey(secret.USER, secret.API_KEY);
+
+client.on('connect', function() {
+    console.log("connected");
+});
+
+client.connect();
+
+client.on('data', function(data) {
+    var results = JSON.parse(data);
+    console.log(results.rows);
+});
+
+client.sql("select * from tracker limit 5");
+client.sql("select * from tracker limit 5 offset 5");
+
+
+// the process do not finish here, client connection is persistent
+// so you have to finish the process manually
+// if you dont call client.connect the connection will not be persistent
+// so the process will finish after the two request finish
+
