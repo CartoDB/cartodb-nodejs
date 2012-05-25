565197ca3e8746d0e3a1feea414012e12a44ff75
Dan Zajdband <dan.zajdband@gmail.com>
2012-05-25 16:02:26 -0300

    first iteration with bugs

--- test/client.oauth.js
+++ test/client.oauth.js
@@ -1,6 +1,6 @@
 
 var assert = require('assert');
-var cartodb = require('../');
+var CartoDB = require('../');
 var secret = require('./secret');
 
 /**
@@ -11,27 +11,26 @@ var shouldBehaveLikeASQLCLient = function() {
     it('should emit data with valid sql', function(done) {
         var sql = "select * from {table} limit 1";
         console.log(secret.EXISTING_TABLE);
-        this.client.sql(sql, {table: secret.EXISTING_TABLE});
+        this.client.query(sql, {table: secret.EXISTING_TABLE});
         this.client.on('data', function(data) {
-            var response = JSON.parse(data);
-            assert.equal(1, response.total_rows);
+            assert.equal(1, data.total_rows);
             done();
         });
     });
 
     it('should emit error with valid invalid sql', function(done) {
         var sql = "select * from {table} limit 1";
-        this.client.sql(sql, {table: 'ASDASDASDASDASDassaDDS'});
-        this.client.on('error', function(data) {
-            assert.ok(data.statusCode !== undefined);
-            assert.ok(JSON.parse(data.data).error !== undefined);
+        this.client.query(sql, {table: 'ASDASDASDASDASDassaDDS'});
+        this.client.on('error', function(error) {
+            assert.ok(typeof error !== undefined);
+            assert.ok(typeof error.statusCode !== undefined);
             done();
         });
     });
 
     it('should be able to do write operations', function(done) {
         var sql = "insert into {table} (name) values ('test')";
-        this.client.sql(sql, {table: secret.EXISTING_TABLE});
+        this.client.query(sql, {table: secret.EXISTING_TABLE});
         this.client.on('data', function(data) {
             done();
         });
@@ -42,12 +41,13 @@ var shouldBehaveLikeASQLCLient = function() {
 
 describe('cartodb_oauth_client', function() {
   beforeEach(function(done){
-    this.client = new cartodb.CartoDBClient(
-        secret.USER,
-        secret.password,
-        secret.CONSUMER_KEY, 
-        secret.CONSUMER_SECRET);
+    this.client = new CartoDB({
+        user : secret.USER,
+        password : secret.password,
+        consumer_key : secret.CONSUMER_KEY, 
+        consumer_secret : secret.CONSUMER_SECRET});
     this.client.connect();
+    console.log('connecting');
     this.client.on('connect', function() {
         done();
     });
