72584bafd5ddea9b7f428e07b3946b98b43a9c09
javi <qualopec@gmail.com>
2013-02-18 09:25:57 +0100

    reverted pipe example

--- examples/pipe.js
+++ examples/pipe.js
@@ -9,7 +9,8 @@ var client = new CartoDB({user:secret.USER, api_key:secret.API_KEY});
 
 client.on('connect', function() {
     client
-        .query("select * from monroecountysnap limit 5");
+      .query("select * from {table} limit 5", {table: 'tracker'})
+      .query("select * from tracker limit 5 offset 5");
 });
 
 client.pipe(file);
