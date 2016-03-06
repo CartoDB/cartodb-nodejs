3128e786b22a17e386df664ffc35c4baec1e77cf
nerik <erik.escoffier@gmail.com>
2016-03-06 14:04:30 +0100

    basic command line support

---
+++ bin/sql.bin.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+var CartoDB = require('../');
+
+var commandLineArgs = require('command-line-args');
+
+var cli = commandLineArgs([
+  { name: 'sql', alias: 's', type: String, defaultOption: true },
+  { name: 'user', alias: 'u', type: String },
+  { name: 'api_key', alias: 'a', type: String },
+  { name: 'format', alias: 'f', type: String },
+  { name: 'output', alias: 'o', type: String },
+  { name: 'help', alias: 'h' }
+])
+
+var cli_opts = cli.parse();
+
+if (cli_opts.help || !cli_opts.sql) {
+  console.log(cli.getUsage());
+  return;
+}
+
+var sql = new CartoDB.SQL({
+  user: cli_opts.user,
+  api_key: cli_opts.api_key
+});
+
+
+var options = {};
+if (cli_opts.format) options.format = cli_opts.format;
+
+sql.execute(cli_opts.sql, options).done(function (data) {
+  if (cli_opts.output) {
+    var file = require('fs').createWriteStream(cli_opts.output);
+    sql.pipe(file);
+  } else {
+    console.log(data)
+  }
+}).error(function (error) {
+  console.log(error)
+})
