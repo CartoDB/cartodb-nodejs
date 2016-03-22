7ff065a385f6e49bdc54daef1763c4d6c909c764
nerik <erik.escoffier@gmail.com>
2016-03-22 17:00:11 +0100

    allow usage of external son config file

--- bin/sql.bin.js
+++ bin/sql.bin.js
@@ -2,44 +2,35 @@
 
 var CartoDB = require('../');
 
-var commandLineArgs = require('command-line-args');
-
 var args = [
-  { name: 'sql', alias: 's', type: String, defaultOption: true, description: 'A SQL query' },
-  { name: 'user', alias: 'u', type: String, description: 'Your CartoDB username' },
-  { name: 'api_key', alias: 'a', type: String, description: 'Your CartoDB API Key (only needed for write operations)' },
+  { name: 'sql', alias: 's', type: String, defaultOption: true, description: 'A SQL query (required)' },
   { name: 'format', alias: 'f', type: String, description: 'Output format json|csv|geojson|shp|svg|kml|SpatiaLite' },
   { name: 'output', alias: 'o', type: String, description: 'Output file. If omitted will use stdout' },
   { name: 'config', alias: 'c', type: String, description: 'Config file.' },
   { name: 'help', alias: 'h' }
 ];
 
-var cli = commandLineArgs(args);
+var options = require('../lib/util/cli.js').getCommandLineArgs(args);
 
-var cli_opts = cli.parse();
-
-if (cli_opts.help || !cli_opts.sql) {
-  console.log(cli.getUsage(args));
+if (options.help || !options.sql) {
+  console.log(options.usage);
   return;
 }
 
 var sql = new CartoDB.SQL({
-  user: cli_opts.user,
-  api_key: cli_opts.api_key
+  user: options.user,
+  api_key: options.api_key
 });
 
-
-var options = {};
-
-if (cli_opts.format) options.format = cli_opts.format;
-
-if (cli_opts.output) {
-  var file = require('fs').createWriteStream(cli_opts.output);
+if (options.output) {
+  var file = require('fs').createWriteStream(options.output);
   sql.pipe(file);
 }
 
-sql.execute(cli_opts.sql, options).done(function (data) {
-  if (!cli_opts.output) console.log(data)
+sql.execute(options.sql, {
+  format: options.format
+}).done(function (data) {
+  if (!options.output) console.log(data)
 }).error(function (error) {
   console.log(error)
-})
+});
