87be3360c62ff7dfdf0ee2cb2eae93c24d0cb834
nerik <erik.escoffier@gmail.com>
2016-03-22 16:30:28 +0100

    completed cartodb-sql cli

--- bin/sql.bin.js
+++ bin/sql.bin.js
@@ -4,23 +4,24 @@ var CartoDB = require('../');
 
 var commandLineArgs = require('command-line-args');
 
-var cli = commandLineArgs([
-  { name: 'sql', alias: 's', type: String, defaultOption: true },
-  { name: 'user', alias: 'u', type: String },
-  { name: 'api_key', alias: 'a', type: String },
-  { name: 'format', alias: 'f', type: String },
-  { name: 'output', alias: 'o', type: String },
+var args = [
+  { name: 'sql', alias: 's', type: String, defaultOption: true, description: 'A SQL query' },
+  { name: 'user', alias: 'u', type: String, description: 'Your CartoDB username' },
+  { name: 'api_key', alias: 'a', type: String, description: 'Your CartoDB API Key (only needed for write operations)' },
+  { name: 'format', alias: 'f', type: String, description: 'Output format json|csv|geojson|shp|svg|kml|SpatiaLite' },
+  { name: 'output', alias: 'o', type: String, description: 'Output file. If omitted will use stdout' },
+  { name: 'config', alias: 'c', type: String, description: 'Config file.' },
   { name: 'help', alias: 'h' }
-])
+];
+
+var cli = commandLineArgs(args);
 
 var cli_opts = cli.parse();
 
 if (cli_opts.help || !cli_opts.sql) {
-  console.log(cli.getUsage());
+  console.log(cli.getUsage(args));
   return;
 }
-
-console.log(cli_opts.api_key)
 
 var sql = new CartoDB.SQL({
   user: cli_opts.user,
@@ -29,16 +30,16 @@ var sql = new CartoDB.SQL({
 
 
 var options = {};
+
 if (cli_opts.format) options.format = cli_opts.format;
 
+if (cli_opts.output) {
+  var file = require('fs').createWriteStream(cli_opts.output);
+  sql.pipe(file);
+}
+
 sql.execute(cli_opts.sql, options).done(function (data) {
-  if (cli_opts.output) {
-    var file = require('fs').createWriteStream(cli_opts.output);
-    sql.pipe(file);
-  } else {
-    console.log(data)
-  }
+  if (!cli_opts.output) console.log(data)
 }).error(function (error) {
-  console.log('ERROR')
   console.log(error)
 })
